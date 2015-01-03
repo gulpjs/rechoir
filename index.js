dd3c8a0e912bedf613db9c14a2f9af849bf65128
silkentrance <trancesilken@gmail.com>
2015-01-03 20:12:09 +0000

    - fixes #10: user must now pass in calling module, uses user module to require() modules/files

--- index.js
+++ index.js
@@ -4,31 +4,41 @@ const interpret = require('interpret');
 
 const EXTRE = /^[.]?[^.]+([.].*)$/;
 
-exports.registerFor = function (filepath, cwd) {
-  var match = EXTRE.exec(path.basename(filepath));
-  if (!match) {
-    return;
-  }
-  var ext = match[1];
-  if (Object.keys(require.extensions).indexOf(ext) !== -1) {
-    return;
-  }
-  if (!cwd) {
-    cwd = path.dirname(path.resolve(filepath));
-  }
-  var moduleName = interpret.extensions[ext];
-  if (moduleName) {
-    var compiler = require(resolve.sync(moduleName, {basedir: cwd}));
-    var register = interpret.register[moduleName];
-    if (register) {
-      register(compiler);
+module.exports = function (mod) {
+
+    var path = mod.require('path');
+
+    var result = {
+      registerFor : function (filepath, cwd) {
+        var match = EXTRE.exec(path.basename(filepath));
+        if (!match) {
+          return;
+        }
+        var ext = match[1];
+        if (Object.keys(require.extensions).indexOf(ext) !== -1) {
+          return;
+        }
+        if (!cwd) {
+          cwd = path.resolve(path.dirname(mod.filename));
+        }
+        var moduleName = interpret.extensions[ext];
+        if (moduleName) {
+          var compiler = mod.require(resolve.sync(moduleName, {basedir: cwd}));
+          var register = interpret.register[moduleName];
+          if (register) {
+            register(compiler);
+          }
+        }
+      },
+
+      load : function (filepath) {
+        result.registerFor(filepath);
+        return mod.require(filepath);
+      },
+
+      interpret : interpret
     }
-  }
+
+    return result;
 }
 
-exports.load = function (filepath) {
-  exports.registerFor(filepath);
-  return require(path.resolve(filepath));
-};
-
-exports.interpret = interpret;
