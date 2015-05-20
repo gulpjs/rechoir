9fe1e7f6c8fca6c4413c9e80c1d7d76172aa1705
Blaine Bublitz <blaine@iceddev.com>
2015-05-20 15:18:11 -0700

    code cleanup

--- test/helpers.js
+++ test/helpers.js
@@ -4,33 +4,37 @@ const semver = require('semver');
 const requireUncached = require('require-uncached');
 
 // save the original Module._extensions
-var original = Object.keys(Module._extensions).reduce(function(result, key){
+const originalExtensions = Object.keys(Module._extensions);
+const original = originalExtensions.reduce(function (result, key) {
   result[key] = require.extensions[key];
   return result;
 }, {});
 // save the original cache keys
-var originalCacheKeys = Object.keys(require.cache);
+const originalCacheKeys = Object.keys(require.cache);
 // save the original Module.prototype.load because coffee-script overwrites it
-var originalModuleLoad = Module.prototype.load;
+const originalModuleLoad = Module.prototype.load;
+
+function cleanupCache(key) {
+  if(originalCacheKeys.indexOf(key) === -1){
+    delete require.cache[key];
+  }
+}
+
+function cleanupExtensions(ext) {
+  if(originalExtensions.indexOf(ext) === -1){
+    delete Module._extensions[ext];
+  } else {
+    Module._extensions[ext] = original[ext];
+  }
+}
 
 function cleanup() {
   // restore the require.cache to startup state
-  Object.keys(require.cache).forEach(function(key){
-    if(originalCacheKeys.indexOf(key) === -1){
-      delete require.cache[key];
-    }
-  });
+  Object.keys(require.cache).forEach(cleanupCache);
   // restore the original Module.prototype.load
   Module.prototype.load = originalModuleLoad;
   // restore the original Module._extensions
-  var extensions = Object.keys(original);
-  Object.keys(Module._extensions).forEach(function(ext){
-    if(extensions.indexOf(ext) === -1){
-      delete Module._extensions[ext];
-    } else {
-      Module._extensions[ext] = original[ext];
-    }
-  });
+  Object.keys(Module._extensions).forEach(cleanupExtensions);
 }
 
 function skippable(module, minVersion, fn) {
